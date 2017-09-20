b7892972bc1936a0e86b048b6f1ae17312b6a1ec
Man Leung <lepusleung@gmail.com>
2017-09-20 15:09:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-var recipes = {};
-function updateObjectWithKeyAndValue(obj,key,value){
-  return Object.assign({},obj,{[key]:value})
+var receipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, obj, [key]:value);
 }
-function destructivelyUpdateObjectWithKeyAndValue(obj,key,value){
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
   obj[key]=value;
   return obj;
 }
+
 function deleteFromObjectByKey(object, key){
-  var temp = Object.assign({},object);
-  delete temp[key];
-  return temp;
+  var tobj = Object.assign({},object);
+  delete tobj[key];
+  return tobj;
 }
 function destructivelyDeleteFromObjectByKey(object, key){
   delete object[key];
